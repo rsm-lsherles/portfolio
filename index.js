13b57bfa3ac8d85f4f6f30f0014d3c00f15abb57
Lucas Sherles <lucasherles@gmail.com>
2026-08-12 12:36:26 -0700

    edit links github

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const projects = await fetchJSON('./lib/projects.json');
 const latestProjects = projects.slice(0, 3);
 const projectsContainer = document.querySelector('.projects');
 renderProjects(latestProjects, projectsContainer, 'h2');
-const githubData = await fetchGitHubData('lsherles');
+const githubData = await fetchGitHubData('rsm-lsherles');
 const profileStats = document.querySelector('#profile-stats');
 if (profileStats) {
     profileStats.innerHTML = `
